343a9f2ad1142d652e07c1f94f315facc9b495a9
Alyona <sergeevaalyonasergeevna@gmail.com>
2026-03-03 01:24:07 +0800

    я написала класс и функцию отвечающую за локику моего задания

--- zadanie2_c#/Program.cs
+++ zadanie2_c#/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System;
+
+namespace zadanie2
+{
+    public class Logic
+    {
+        public static int Multiply(int x, int y)
+        {
+            int res = 0;
+
+            for (int i = 0; i < y; i++)
+            {
+                res = res + x;
+            }
+
+            return res;
+        }
+    }
+
+    
+}
